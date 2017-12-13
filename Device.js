d91bdbc62b3b263c7996bf0bf1f415d95f2d0be5
angery caillou <31480869+8jsalamone@users.noreply.github.com>
2017-12-13 10:08:19 -0600

    Add files via upload

--- Device.js
+++ Device.js
@@ -1,5 +1,3 @@
-//Defines how all Devices with work.
-
 function Device(t,ma,c){
 
     //Instance Variables
@@ -11,20 +9,28 @@ function Device(t,ma,c){
     this.rate = [0.0015,0.0235,0.23];
 
     //Instance Functions
+    this.power = function(){
+        return this.juice
+    };
+
     this.off = function(){
         if(this.state == "active" || this.state == "idle" || this.juice <= 0)
-           this.state = "off";
+        this.state = "off";
     };
 
     this.on = function(){
         if(this.state == "off" && this.juice >0){
-           this.state = "idle";
-       }
+            this.state = "idle";
+        }
     };
-
+    this.sleep = function(){
+        if(this.state == "active" && this.juice > 0){
+            this.state = "idle"
+        }
+    }
     this.wake = function(){
         if(this.state == "off" || this.state == "idle"){
-            this.state == "active"
+            this.state = "active"
         }
     };
 
@@ -32,30 +38,31 @@ function Device(t,ma,c){
         let time = min / 60;
 
         if(this.state == "off"){
-            let juice = juice - this.rate[0];
+             this.juice = this.juice - this.rate[0];
         }
         else if(this.state=="idle"){
-            let juice = juice - this.rate[1];
+             this.juice = this.juice - this.rate[1];
         }
         else if(this.state == "active"){
-            let juice = juice - this.rate[2];
+             this.juice = this.juice - this.rate[2];
         }
     }
 
     this.charge = function(min){
         //adds more electricity to the device's juice depending on its state
+        let output
         let time = min / 60;
         let charge = (this.millAmps / this.capacity);
         if(this.state == "off"){
-            let output = 1 - this.rate[0];
+            output = 1 - this.rate[0];
         }
         else if(this.state=="idle"){
-            let output = 1 - this.rate[1];
+            output = 1 - this.rate[1];
         }
         else if(this.state == "active"){
-            let output = 1 - this.rate[2];
+            output = 1 - this.rate[2];
         }
-this.juice = this.juice + charge*output*time;
+        this.juice = this.juice + charge*output*time;
         //resets juice to 1 if it has exceeded 1
         if(this.juice > 1){
             this.juice = 1
@@ -66,6 +73,23 @@ this.juice = this.juice + charge*output*time;
 }//end of the device declaration
 
 //defines the testing code.
-function main(){}
-//runs the testing code.
+function main(){
+let flemphone = new Device("phone",3000,10000);
+console.log(flemphone.power());
+flemphone.on();
+flemphone.wake();
+flemphone.use(90);
+console.log(flemphone.power());
+flemphone.sleep();
+flemphone.use(300);
+console.log(flemphone.power());
+flemphone.off();
+flemphone.use(300);
+console.log(flemphone.power());
+flemphone.charge(60);
+console.log(flemphone.power());
+flemphone.charge(1000);
+console.log(flemphone.power());
+}
 main();
+//Defines how all Devices with work.
